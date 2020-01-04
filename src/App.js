01145b4dfb7c0ae9b26b7695c283fbb3d0f6f512
hanenWahabi <hanenouahabii@gmail.com>
2020-01-04 08:17:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
-import { Description } from "./components/Description";
-import { SearchBar } from "./components/SearchBar";
-import { Statistic } from "./components/Statistic";
-import { Footer } from "./components/Footer";
-
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Description />
-      <SearchBar />
-      <Statistic />
-      <Footer />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
