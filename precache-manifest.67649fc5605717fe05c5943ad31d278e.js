b755580b1524c78eafe6c3ccca60cbc7f79e29d8
hanenWahabi <hanenouahabii@gmail.com>
2020-12-15 18:41:07 +0100

    Updates

---
+++ precache-manifest.67649fc5605717fe05c5943ad31d278e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4692b84af31beb5ffe4e45bf6f32db",
+    "url": "/Shortly/index.html"
+  },
+  {
+    "revision": "230d9bddc50d71dfcacf",
+    "url": "/Shortly/static/css/main.3d374d01.chunk.css"
+  },
+  {
+    "revision": "6a6f87f1bae4e59019e5",
+    "url": "/Shortly/static/js/2.32c910b8.chunk.js"
+  },
+  {
+    "revision": "230d9bddc50d71dfcacf",
+    "url": "/Shortly/static/js/main.e8a762a4.chunk.js"
+  },
+  {
+    "revision": "0a1e84caa94077a5ce13",
+    "url": "/Shortly/static/js/runtime-main.4240e247.js"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/Shortly/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/Shortly/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/Shortly/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/Shortly/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/Shortly/static/media/illustration-working.5029d049.svg"
+  }
+]);
